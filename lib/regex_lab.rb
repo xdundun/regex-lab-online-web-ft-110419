8047ed90175cef689053a1d2251f52ac8d884cd2
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-10 19:57:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
   if word.match(/\A[^aeiou]/)
-    return true
+    return false
  else
-   return false
+   return true
  end
 end
 def words_starting_with_un_and_ending_with_ing(text)
